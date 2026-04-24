ea010a00a10fcaafe9aafe40d77a3e14be1b17b6
Yogesh <sharmayogesh152004@gmail.com>
2026-04-24 18:07:37 +0530

    day-16

--- day-14/src/controllers/post.controller.js
+++ day-14/src/controllers/post.controller.js
@@ -1,6 +1,7 @@
 const postModel = require("../models/post.model")
 const ImageKit = require("@imagekit/nodejs");
 const {toFile} = require("@imagekit/nodejs")
+const jwt = require("jsonwebtoken")
 
 const imagekit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
@@ -9,12 +10,40 @@ const imagekit = new ImageKit({
 async function createPostController (req,res) {
   console.log(req.body , req.file);
 
+  const token = req.cookies.token
+
+  if(!token){
+    return res.status(401).json({
+      message:"Token not provided , unauthorized access"
+    })
+  }
+
+  let decoded = null
+
+  try {
+     decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+     return res.status(401).json({
+      message:"user not authorized"
+     })
+  }
+
   const file =  await imagekit.files.upload({
     file:await toFile(Buffer.from(req.file.buffer) , 'file'),
-    fileName:"Test"
+    fileName:"Test",
+    folder:"cohort-InstaClone-posts"
   })
   
-  res.send(file)
+ const post = await postModel.create({
+   caption:req.body.caption,
+   imgUrl:file.url,
+   user:decoded.id
+ })
+
+ res.status(201).json({
+  message:"Post created sucessfully",
+  post
+ })
 
 }
 
